56b8f2d9be916aa87cfeec5cb2ff030fb9a2bf87
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2021-06-26 10:47:56 +0000

    CinetPayController updated

--- src/Controller/BackendPaiementController.php
+++ src/Controller/BackendPaiementController.php
@@ -31,8 +31,8 @@ class BackendPaiementController extends AbstractController
      */
     public function index($statut): Response
     {
-        if ($statut === 'VALID') $statutLibelle = 'valides';
-        else $statutLibelle = 'non valides';
+        if ($statut === 'VALID') $statutLibelle = 'validées';
+        else $statutLibelle = 'non achevées';
 
         return $this->render('backend_paiement/index.html.twig', [
             'listes' => $this->utility->listPaiement($statut),
@@ -48,7 +48,17 @@ class BackendPaiementController extends AbstractController
         $config = $this->getDoctrine()->getRepository(Config::class)->findOneBy(['region'=>$regionId]);
 
         if ($config){
-            return $this->redirect('http://sicre.scoutascci.org/cinetpay/notify/'.$config->getSiteId().'?cpm_trans_id='.$idTransaction);
+            //$siteId = 859470; $id_transaction = '20210610175018-60c250da8dbb3';
+            //$siteId = $config->getSiteId(); $id_transaction = $idTransaction;
+            //return $this->redirect('http://sicre.scoutascci.org/cinetpay/notify/859470?cpm_trans_id=20210610175018-60c250da8dbb3');
+            //$old = 'http://sicre.scoutascci.org/cinetpay/notify/'.$config->getSiteId().'?cpm_trans_id='.$idTransaction; dd($old);
+            //$url = "http://sicre.scoutascci.org/cinetpay/notify/859470?cpm_trans_id=20210610175018-60c250da8dbb3";
+            //$url = "http://sicre.scoutascci.org/cinetpay/notify/".$siteId."?cpm_trans_id=".$id_transaction;
+
+            $url = sprintf('http://sicre.scoutascci.org/cinetpay/notify/%d?cpm_trans_id=%s', $config->getSiteId(), $idTransaction);
+            //die($url);
+
+            return $this->redirect($url);
         }
 
         return $this->redirectToRoute('backend_paiement_index',['statut'=>'UNKNOW']);
